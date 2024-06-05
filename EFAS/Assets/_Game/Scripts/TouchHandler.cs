49c007537f418c464276c82a3790d6272696d2b7
atm <144315333+nmtuonggh@users.noreply.github.com>
2024-06-06 00:17:22 +0700

    [[

--- EFAS/Assets/_Game/Scripts/TouchHandler.cs
+++ EFAS/Assets/_Game/Scripts/TouchHandler.cs
@@ -1,27 +1,43 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TouchHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class TouchHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-    private Vector2 dragStartPosition;
-    private bool isDragging = false;
-
-    public void OnBeginDrag(PointerEventData eventData)
+    private bool isTouching = false;
+    private Vector2 previousTouchPosition;
+    public Transform cameraFollow;
+    float rotationSpeed = 0.1f;
+    
+    public void OnPointerDown(PointerEventData eventData)
     {
-        dragStartPosition = eventData.position;
-        isDragging = true;
+        isTouching = true;
+        previousTouchPosition = eventData.position;
     }
-
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isTouching = false;
+    }
+    
     public void OnDrag(PointerEventData eventData)
     {
-        if (isDragging)
+        if (isTouching)
         {
-            
+            Vector2 currentTouchPosition = eventData.position;
+            Vector2 deltaPosition = currentTouchPosition - previousTouchPosition;
+            Debug.Log(previousTouchPosition + " " + currentTouchPosition);
+            RotateCamera(deltaPosition);
+            previousTouchPosition = currentTouchPosition;
         }
     }
-
-    public void OnEndDrag(PointerEventData eventData)
+    
+    private void RotateCamera(Vector2 deltaPosition)
     {
-        isDragging = false;
+        float deltaX = deltaPosition.x * rotationSpeed;
+        float deltaY = deltaPosition.y * rotationSpeed;
+        
+        cameraFollow.Rotate(Vector3.right, deltaX, Space.World);
+        cameraFollow.Rotate(Vector3.up, -deltaY, Space.World);
+        //cameraFollow.transform.rotation = Quaternion.Euler(deltaX, - deltaY, 0);
     }
 }
