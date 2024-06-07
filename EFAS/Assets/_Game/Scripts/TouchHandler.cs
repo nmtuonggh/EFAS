0783089d0a0b5fae0f931c3afabf0624594d5669
nmtuonggh <tuongwork@gmail.com>
2024-06-07 18:15:35 +0700

    no thing

--- EFAS/Assets/_Game/Scripts/TouchHandler.cs
+++ EFAS/Assets/_Game/Scripts/TouchHandler.cs
@@ -9,22 +9,16 @@ public class TouchHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
     
     public void OnPointerDown(PointerEventData eventData)
     {
-        //Debug.Log("On Start PDown =>" + "isT : " + isTouching + "|| id : " + activeFingerId);
-
         if (!isTouching || activeFingerId == -1)
         {
             isTouching = true;
             previousTouchPosition = eventData.position;
             activeFingerId = eventData.pointerId;
         }
-        //Debug.Log("On End PDown =>" + "isT : " + isTouching + "|| id : " + activeFingerId);
-
     }
     
     public void OnDrag(PointerEventData eventData)
     {
-        //Debug.Log("OnStartPoiDrag => isT : " + isTouching + "|| id : " + activeFingerId +"|| evenID: "+ eventData.pointerId);
-
         if (isTouching && eventData.pointerId == activeFingerId)
         {
             var currentTouchPosition = eventData.position;
@@ -33,33 +27,22 @@ public class TouchHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
             CameraController.Instance.CameraRotation(deltaPosition);
             previousTouchPosition = currentTouchPosition;
         }
-        //Debug.Log("OnEndPoiDrag => isT : " + isTouching + "|| id : " + activeFingerId +"|| evenID: "+ eventData.pointerId);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        //Debug.Log("OnStart PUp => isT : " + isTouching + "| id : " + activeFingerId +"| evenID: "+ eventData.pointerId);
-
         if (eventData.pointerId == activeFingerId)
         {
-            //reset id when 1st finger up
             activeFingerId = -1;
             
-            // check other finger
             if (Input.touchCount > 0)
             {
-                //Debug.Log("|touch count : " + Input.touchCount);
-
                 for (int i = 0; i < Input.touchCount; i++)
                 {
                     Touch touch = Input.GetTouch(i);
                     if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
                     {
-                        //Debug.Log("Touch index = " + i);
-                        //activeFingerId = touch.fingerId -1;
                         activeFingerId = touch.fingerId;
-                        //Debug.Log("|fingerid : " + touch.fingerId);
-
                         previousTouchPosition = touch.position;
                         break;
                     }
@@ -70,6 +53,5 @@ public class TouchHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
                 isTouching = false;
             }
         }
-        //Debug.Log("OnEndPUp => isT : " + isTouching + "| id : " + activeFingerId +"| evenID: "+ eventData.pointerId);
     }
 }
