62e1fd0e74aac4579f2a1a7e10d8472c45b9982c
nmtuonggh <tuongwork@gmail.com>
2024-06-13 18:02:51 +0700

    add pack

---
+++ EFAS/Assets/_Game/_Script/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using EasyCharacterMovement;
+using UnityEngine;
+
+public class Player : Character
+{
+    protected void HandlerInput()
+    {
+        
+    }
+}
